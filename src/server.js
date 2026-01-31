4c8e95b342d381b7230683f91b4047e9767b57f1
shaysiboni1-alt <shaysiboni1@gmail.com>
2026-01-31 05:44:05 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
+// src/server.js
+"use strict";
+
+const express = require("express");
+const { env } = require("./config/env");
+const { logger } = require("./utils/logger");
+const { healthRouter } = require("./routes/health");
+
+const app = express();
+
+app.use(express.json({ limit: "1mb" }));
+
+app.use(healthRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ error: "not_found" });
+});
+
+app.listen(env.PORT, () => {
+  logger.info("Service started", { port: env.PORT, provider_mode: env.PROVIDER_MODE });
+});
 
