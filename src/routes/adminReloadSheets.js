d27603cc7dc8908ca0a5ae892ab04a52c064cd8b
shaysiboni1-alt <shaysiboni1@gmail.com>
2026-01-31 05:50:01 +0200

    Update adminReloadSheets.js

--- src/routes/adminReloadSheets.js
+++ src/routes/adminReloadSheets.js
@@ -1,1 +1,32 @@
+// src/routes/adminReloadSheets.js
+"use strict";
+
+const express = require("express");
+const { loadSSOT } = require("../ssot/ssotClient");
+const { env } = require("../config/env");
+
+const router = express.Router();
+
+router.post("/admin/reload-sheets", async (req, res) => {
+  const token = req.headers["x-admin-token"];
+
+  if (!token || token !== env.TWILIO_AUTH_TOKEN) {
+    return res.status(401).json({ error: "unauthorized" });
+  }
+
+  try {
+    const ssot = await loadSSOT(true);
+    return res.status(200).json({
+      reloaded: true,
+      loaded_at: ssot.loaded_at
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: "reload_failed",
+      message: err.message
+    });
+  }
+});
+
+module.exports = { adminReloadRouter: router };
 
