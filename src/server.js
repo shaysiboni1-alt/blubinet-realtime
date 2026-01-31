d0934a59678cd81f2059556f26436008f6414948
shaysiboni1-alt <shaysiboni1@gmail.com>
2026-01-31 05:50:19 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,18 +5,30 @@ const express = require("express");
 const { env } = require("./config/env");
 const { logger } = require("./utils/logger");
 const { healthRouter } = require("./routes/health");
+const { adminReloadRouter } = require("./routes/adminReloadSheets");
+const { loadSSOT } = require("./ssot/ssotClient");
 
 const app = express();
 
 app.use(express.json({ limit: "1mb" }));
 
 app.use(healthRouter);
+app.use(adminReloadRouter);
 
 app.use((req, res) => {
   res.status(404).json({ error: "not_found" });
 });
 
-app.listen(env.PORT, () => {
-  logger.info("Service started", { port: env.PORT, provider_mode: env.PROVIDER_MODE });
-});
+app.listen(env.PORT, async () => {
+  logger.info("Service started", {
+    port: env.PORT,
+    provider_mode: env.PROVIDER_MODE
+  });
 
+  // Best-effort preload SSOT
+  try {
+    await loadSSOT(false);
+  } catch (err) {
+    logger.error("SSOT preload failed", { error: err.message });
+  }
+});
