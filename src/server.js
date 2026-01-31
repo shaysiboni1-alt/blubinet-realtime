bc779514a025f014345070399074b0ff6c04fe52
shaysiboni1-alt <shaysiboni1@gmail.com>
2026-01-31 20:47:27 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-// src/server.js
 "use strict";
 
 const http = require("http");
@@ -7,29 +6,28 @@ const { env } = require("./config/env");
 const { logger } = require("./utils/logger");
 const { healthRouter } = require("./routes/health");
 const { adminReloadRouter } = require("./routes/adminReloadSheets");
+const { twilioStatusRouter } = require("./routes/twilioStatus");
 const { loadSSOT } = require("./ssot/ssotClient");
-const { attachTwilioMediaWs } = require("./telephony/twilioMediaWs");
+const { installTwilioMediaWs } = require("./ws/twilioMediaWs");
 
 const app = express();
 app.use(express.json({ limit: "1mb" }));
 
 app.use(healthRouter);
 app.use(adminReloadRouter);
+app.use(twilioStatusRouter);
 
 app.use((req, res) => res.status(404).json({ error: "not_found" }));
 
 const server = http.createServer(app);
-
-// WebSocket: /twilio-media-stream
-attachTwilioMediaWs(server);
+installTwilioMediaWs(server);
 
 server.listen(env.PORT, async () => {
   logger.info("Service started", {
     port: env.PORT,
-    provider_mode: env.PROVIDER_MODE,
+    provider_mode: env.PROVIDER_MODE
   });
 
-  // Best-effort preload SSOT
   try {
     await loadSSOT(false);
   } catch (err) {
