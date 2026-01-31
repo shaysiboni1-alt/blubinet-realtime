a527a52b0d77cb78f926684c96cb8aa4dac22cd1
shaysiboni1-alt <shaysiboni1@gmail.com>
2026-01-31 06:49:59 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,14 +6,18 @@ const { env } = require("./config/env");
 const { logger } = require("./utils/logger");
 const { healthRouter } = require("./routes/health");
 const { adminReloadRouter } = require("./routes/adminReloadSheets");
+const { twilioStatusRouter } = require("./routes/twilioStatus");
 const { loadSSOT } = require("./ssot/ssotClient");
 
 const app = express();
 
+// Twilio sends application/x-www-form-urlencoded by default
+app.use(express.urlencoded({ extended: false, limit: "1mb" }));
 app.use(express.json({ limit: "1mb" }));
 
 app.use(healthRouter);
 app.use(adminReloadRouter);
+app.use(twilioStatusRouter);
 
 app.use((req, res) => {
   res.status(404).json({ error: "not_found" });
