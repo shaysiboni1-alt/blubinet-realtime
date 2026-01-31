5e70b1ee4395fd9e14160f9fd6bd89222c739543
shaysiboni1-alt <shaysiboni1@gmail.com>
2026-01-31 07:07:38 +0200

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,19 +1,25 @@
-// src/utils/logger.js
-function base(level, msg, meta) {
-  const time = new Date().toISOString();
-  if (meta !== undefined) {
-    console.log(JSON.stringify({ time, level, msg, meta }));
-  } else {
-    console.log(JSON.stringify({ time, level, msg }));
-  }
+"use strict";
+
+const { env } = require("../config/env");
+
+function log(level, msg, meta) {
+  const line = {
+    time: new Date().toISOString(),
+    level,
+    msg
+  };
+  if (meta && typeof meta === "object") line.meta = meta;
+  // JSON lines for Render
+  console.log(JSON.stringify(line));
 }
 
 const logger = {
-  info: (msg, meta) => base("info", msg, meta),
-  warn: (msg, meta) => base("warn", msg, meta),
-  error: (msg, meta) => base("error", msg, meta),
-  debug: (msg, meta) => base("debug", msg, meta)
+  info: (msg, meta) => log("info", msg, meta),
+  warn: (msg, meta) => log("warn", msg, meta),
+  error: (msg, meta) => log("error", msg, meta),
+  debug: (msg, meta) => {
+    if (env.MB_DEBUG) log("debug", msg, meta);
+  }
 };
 
 module.exports = { logger };
-
